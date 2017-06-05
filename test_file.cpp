dae6ca8b1b5ee86a363f00c4c1f90714de75e215
XSAM <xsambundy@gmail.com>
2017-06-05 10:48:00 +0800

    test: add stringify from file

--- test_file.cpp
+++ test_file.cpp
@@ -41,13 +41,23 @@ void TestParseFromFile()
     value = get<1>(result);
 
     auto end = chrono::high_resolution_clock::now();
+    auto parseElapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+
+    //stringify
+    start = chrono::high_resolution_clock::now();
+
+    auto stringifyResult = bubbleJson.Stringify(value, StringifyType_Beauty);
+
+    end = chrono::high_resolution_clock::now();
+    auto stringifyElapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
 
     //check result
     if(get<0>(result) != ParseResult_Ok)
         fprintf(stderr, "%s:%d: expect: 0 actual: %d", __FILE__, __LINE__, get<0>(result));
 
-    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
-    cout<<"TestParseFromFile() elapsed time: "<<elapsed.count()<<" ms"<<endl;
+    cout<<"parse from file elapsed time: "<<parseElapsed.count()<<" ms"<<endl;
+    cout<<"stringify from file elapsed time: "<<stringifyElapsed.count()<<" ms"<<endl;
 
     delete value;
+    free (get<0>(stringifyResult));
 }
